f3363c18292e0cc8194aca28a409d17333dac679
Jacob Brehm <jake.m.brehm@gmail.com>
2019-11-22 19:54:43 -0500

    Reorganized code into logical functions.

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import praw
 import re
 import requests
 
+def verify_response(url):
+    try:
+        response = requests.get(url, stream=True)
+    except Exception as e:
+        print(e)
+        return False
+    else:
+        if response.status_code == 200:
+            return response
+        else:
+            return False
+
+def download(response, filename):
+    with open(f'img\\{filename}', 'wb') as output:
+        for chunk in response:
+            output.write(chunk)
+
 parser = configparser.ConfigParser()
 parser.read(r'config.ini')
 
@@ -20,29 +37,14 @@ posts = profile.submissions.new(limit=None)
 for post in posts:
     link = str(post.url)
     if link.endswith('.jpg') or link.endswith('.png'):
-        try:
-            response = requests.get(link, stream=True)
-        except Exception as e:
-            print(e)
-            continue
-        else:
-            if response.status_code == 200:
-                filename = link.split('/')[-1]
+        response = verify_response(link)
+        if response:
+            filename = link.split('/')[-1]
+            download(response, filename)
     elif 'imgur.com/a/' in link:
-        link += '/zip'
-        try:
-            response = requests.get(link, stream=True)
-        except Exception as e:
-            print(e)
-            continue
-        else:
-            if response.status_code == 200:
-                disposition = response.headers['Content-Disposition']
-                matches = re.search(r'filename="(.*)"', disposition)
-                filename = matches.group(1)
-    else:
-        continue
-
-    with open(f'img\\{filename}', 'wb') as output:
-        for chunk in response:
-            output.write(chunk)
+        response = verify_response(f'{link}/zip')
+        if response:
+            disposition = response.headers['Content-Disposition']
+            matches = re.search(r'filename="(.*)"', disposition)
+            filename = matches.group(1)
+            download(response, filename)
